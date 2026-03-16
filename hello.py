ff18374fb6d0ee2ac59c1f78d2d4567ddf24635f
Hariom Singh <suppraaaa2003@gmail.com>
2026-03-16 14:25:54 +0530

    using dictionaly fid the temp fromdictanery using lib and api

--- hello.py
+++ hello.py
@@ -1,10 +1,2 @@
+import pandas as pd 
 
-import random
-
-numbers = random.randint(1,10)
-choice = random.choice(["Apple", "banana", "orange"])
-
-
-import datetime
-today = datetime.date.today()
-print( today.year )  # 2024-01-15
